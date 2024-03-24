12a4b221805e21ca3b80124938b8868a3febe715
andynguyen4267 <andynguyen4267@gmail.com>
2024-03-24 16:00:04 -0700

    first commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -17,7 +17,7 @@ export const Hero = () => {
         </a>
       </div>
       <img
-        src={getImageUrl("hero/heroImage.png")}
+        src={getImageUrl("hero/person.png")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
